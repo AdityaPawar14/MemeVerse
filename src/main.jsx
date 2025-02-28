a261dae87555dc98ba9e63ac0ed3bb6a1474c86f
Aditya Pawar <aadipawar1403@gmail.com>
2025-02-28 14:33:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// Find the root element in the DOM
+
 const rootElement = document.getElementById('root');
 
-// Ensure the root element exists before rendering the app
+
 if (rootElement) {
   const root = createRoot(rootElement);
 
-  // Render the app inside StrictMode for additional development checks
+
   root.render(
     <StrictMode>
       <App />
