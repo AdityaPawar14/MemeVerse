2b10b63e6f9c3698a0d0c3c3a3ac248c00ced30f
Aditya Pawar <aadipawar1403@gmail.com>
2025-03-19 13:07:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import userReducer from './slices/userSlice';
 
 export const store = configureStore({
   reducer: {
-    memes: memesReducer, 
-    user: userReducer,   
+    memes: memesReducer,
+    user: userReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      
-      serializableCheck: false,
-    }),
-  
-  devTools: process.env.NODE_ENV !== 'production',
 });
-
-export default store;
